f2a170182c1ad128603f089fd92343fb1e6425eb
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2026-07-13 14:12:30 +0530

    feat: update resume URLs to point to the correct file

--- src/config/Resume.ts
+++ src/config/Resume.ts
@@ -1,5 +1,5 @@
 export const resumeConfig = {
-  url: "https://drive.google.com/file/d/1TkJIQlkwhhK5fRTInvMHB_sgFdFMyLkL/preview",
+  url: "https://drive.google.com/file/d/1i3_bIn1sMWYs-Vojk6eU7524V07hl4rh/preview",
   downloadUrl:
-    "https://drive.google.com/uc?export=download&id=1TkJIQlkwhhK5fRTInvMHB_sgFdFMyLkL",
+    "https://drive.google.com/uc?export=download&id=1i3_bIn1sMWYs-Vojk6eU7524V07hl4rh",
 };
